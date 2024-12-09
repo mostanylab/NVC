079b68af544597e59ca67e6ec2d4aeb7921f7f30
mostanylab <37631335+mostanylab@users.noreply.github.com>
2024-12-09 10:46:12 -0600

    Delete getdir.m

--- getdir.m
+++
@@ -1,8 +0,0 @@
-%This program will set the working directory, so you could call it and
-%change the folder while you are working
-
-directory_name = uigetdir('','Set Pathname')
-
-% backslash = '\*.tif';
-
-dir_name_bslash = strcat(directory_name,'\*.tif');
